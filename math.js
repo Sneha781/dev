081f3776e46d61feabb903c09154d23a7f137413
Sneha781 <snehalatha.ikutti@qwikspark.com>
2024-12-22 10:25:24 +0530

    Update math.js

--- math.js
+++ math.js
@@ -1,10 +1,14 @@
-function add(a, b) {
-    return a + b;
-}
+// Import math.js (for Node.js)
+const math = require('mathjs');
 
-// New function
-function subtract(a, b) {
-    return a - b;
-}
+// Addition
+console.log(math.add(2, 3));  // 5
 
-module.exports = { add, subtract };
+// Subtraction
+console.log(math.subtract(7, 5));  // 2
+
+// Multiplication
+console.log(math.multiply(4, 6));  // 24
+
+// Division
+console.log(math.divide(9, 3));  // 3
