24fe19ffa31fde3ebdf0cca7bad9f98f8d0524f0
Sneha781 <snehalatha.ikutti@qwikspark.com>
2024-12-23 12:05:02 +0530

    Update math.js

--- math.js
+++ math.js
@@ -1,12 +1,7 @@
-// Matrix creation and basic operations
-let matrixA = math.matrix([[1, 2], [3, 4]]);
-let matrixB = math.matrix([[5, 6], [7, 8]]);
-
-// Matrix addition
-let matrixSum = math.add(matrixA, matrixB);
-console.log(`Matrix Addition: ${math.format(matrixSum)}`);
-
-// Matrix multiplication
-let matrixProduct = math.multiply(matrixA, matrixB);
-console.log(`Matrix Multiplication: ${math.format(matrixProduct)}`);
+module.exports = {
+  testMatch: [
+    '**/tests/**/*.test.js', // Search in tests folder
+    '**/?(*.)+(spec|test).js', // Or match .spec.js or .test.js files
+  ],
+};
 
